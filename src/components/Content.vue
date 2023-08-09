ebbdb08f7506083cbb8fc8bcf09ec8f02c37eb93
adim2222 <nowak.adam2222@gmail.com>
2023-08-09 10:14:28 +0200

    added back button to content sites

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -7,6 +7,10 @@ const route = useRoute();
 console.log(route.params);
 
 let id = parseInt(route.params.id)
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
 </script>
 
 <template>
@@ -16,11 +20,28 @@ let id = parseInt(route.params.id)
     <div class="body">
         {{ store.state.data[id - 1].body }}
     </div>
+    <div class="back-button" @click="router.back">
+        Back
+    </div>
 </template>
 
 <style scoped>
 * {
     font-family: Arial, Helvetica, sans-serif;
+}
+.back-button {
+    margin: 20px;
+    width: 200px;
+    height: 50px;
+    background-color: red;
+    text-align: center;
+    font-size: 45px;
+    transition: 0.3s;
+    border-radius: 10px;
+    border: solid black 1px;
+}
+.back-button:hover {
+    transform: translate(10px, 0px);
 }
 header {
     margin: 20px;
